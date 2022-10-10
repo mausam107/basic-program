d4bc1888a231248d1bebae97554fb899889e20c6
mausam107 <mausam2001kv@gmail.com>
2022-10-10 20:03:11 +0530

    code

---
+++ test.js
@@ -0,0 +1,38 @@
+
+
+// function fac(n){
+//     if(n === 1){
+//         return 1;
+//     }
+//     return n*fac(n-1);
+// }
+
+// var factorial= fac(4);
+// console.log(factorial);
+
+let arr = [
+    [1, 2],
+    [3, 4, 5],
+    [6, 7, 8, 9, [12, 23, 21], [11, 33, 44, 55], [100]],
+    [99]
+  ];
+let arr2=[]
+for(let i=0;i<arr.length;i++){
+    for(let j=0;j<arr[i].length;j++){
+        // arr2.push(arr[i][j]);
+        if(!Array.isArray(arr[i][j])){
+            arr2.push(arr[i][j]);
+
+            }
+        if(Array.isArray(arr[i][j])){
+            for(let k=0;k<arr[i][j].length;k++){
+                arr2.push(arr[i][j][k])
+            }
+
+        }
+        
+        
+    }
+    
+}
+console.log(arr2);
